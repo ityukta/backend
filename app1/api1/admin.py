e33f063c366adf7aa01591005ae4b295c230f12f
kalpajpise <kalpaj12@gmail.com>
2020-02-24 21:00:24 +0530

    Fixed Error

--- app1/api1/admin.py
+++ app1/api1/admin.py
@@ -1,26 +1,20 @@
+
 from django.contrib import admin
 
 # Register your models here.
-from .models import(Class)
-    admin.site.register(Class)
+from .models import *
+admin.site.register(Class)
 
-from .models import(Student)
-    admin.site.register(Student)
+admin.site.register(Student)
 
-from .models import(Subject)
-    admin.site.register(Subject)
+admin.site.register(Subject)
 
-from .models import(fcs)
-    admin.site.register(fcs)
+admin.site.register(fcs)
 
-from .models import(fcss)
-    admin.site.register(fcss)
+admin.site.register(fcss)
 
-from .models import(Attendance)
-    admin.site.register(Attendance)
+admin.site.register(Attendance)
 
-from .models import(Tests)
-    admin.site.register(Tests)
+admin.site.register(Tests)
 
-from .models import(Test_res)
-    admin.site.register(Test_res)
+admin.site.register(Test_res)
