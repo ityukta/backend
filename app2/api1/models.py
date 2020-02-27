cc5cb7c828251ea4191470552095cdb1400a1244
Amey Aditya Achar J <ameyaditya.j@gmail.com>
2020-02-27 22:25:31 +0530

    added initial register endpoint

--- app2/api1/models.py
+++ app2/api1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Type(models.Model):
     class TypeChoices(models.TextChoices):
         PRINCIPAL = 'P', _('Principal')
@@ -9,55 +10,8 @@ class Type(models.Model):
         SUBJECT_TEACHER = 'S', _('Subject Teacher')
         MENTOR = 'M', _('Mentor')
     type_id = models.AutoField(primary_key=True)
-    type_description = models.CharField(max_length=1, choices=TypeChoices.choices)
-
-class Faculty(models.Model):
-    class GenderChoices(models.TextChoices):
-        MALE = 'M', _('Male')
-        FEMALE = 'F', _('Female')
-        OTHERS = 'O', _('Others')
-
-    class MartialStatusChoices(models.TextChoices):
-        SINGLE = 'SI', _('Single')
-        MARRIED = 'MA', _('Married')
-        DIVORCED = 'DI', _('Divorced')
-        WIDOWED = 'WI', _('Widowed')
-        SEPERATED = 'SE', _('Seperated')
-
-    class AssociationChoices(models.TextChoices):
-        REGULAR = 'R', _('Regular')
-        VISITING = 'V', _('Visiting')
-        ADJUNCT = 'A', _('Adjunct')
-        CONTRACT = 'C', _('Contract')
-
-    faculty_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email_id = models.EmailField(max_length=100)
-    password = models.CharField(max_length=128)
-    department = models.CharField(max_length=100)
-    phone = models.BigIntegerField(null=True, blank=True)
-    date_of_joining = models.DateField(null=True, blank=True)
-    experience = models.IntegerField(null=True, blank=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=GenderChoices.choices, null=True, blank=True)
-    marital_status = models.CharField(
-        max_length=50, choices=MartialStatusChoices.choices, null=True, blank=True)
-    address = models.TextField(null=True, blank=True)
-    teacher_picture = models.ImageField(null=True, blank=True)
-    designation = models.CharField(max_length=100, null=True, blank=True)
-    association_with_institution = models.CharField(
-        max_length=20, choices=AssociationChoices.choices, null=True, blank=True)
-    faculty_type = models.ManyToManyField(Type)
-    deleted = models.BooleanField(default=False, null=True, blank=True)
-    approved = models.BooleanField(default=False)
-    first_login = models.BooleanField(default=True)
-
-
-class LoginAuthKey(models.Model):
-    login_authkey_id = models.AutoField(primary_key=True)
-    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
-    authkey = models.CharField(max_length=128)
-    deleted = models.BooleanField(default=False)
+    type_description = models.CharField(
+        max_length=1, choices=TypeChoices.choices)
 
 
 class Publications(models.Model):
@@ -71,6 +25,7 @@ class Workshops(models.Model):
     faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     workshop_detail = models.TextField()
 
+
 class FacultyQualification(models.Model):
     qualification_id = models.AutoField(primary_key=True)
     faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
@@ -78,7 +33,7 @@ class FacultyQualification(models.Model):
     branch = models.CharField(max_length=100)
     institution = models.CharField(max_length=100)
     percentage = models.FloatField(max_length=4)
-    graduation_year = models.PositiveSmallIntegerField(null = True)
+    graduation_year = models.PositiveSmallIntegerField(null=True)
     university = models.CharField(max_length=100)
 
 
@@ -114,6 +69,61 @@ class AcademicRole(models.Model):
     academic_role_details = models.TextField()
 
 
+class Faculty(models.Model):
+    class GenderChoices(models.TextChoices):
+        MALE = 'M', _('Male')
+        FEMALE = 'F', _('Female')
+        OTHERS = 'O', _('Others')
+
+    class MartialStatusChoices(models.TextChoices):
+        SINGLE = 'SI', _('Single')
+        MARRIED = 'MA', _('Married')
+        DIVORCED = 'DI', _('Divorced')
+        WIDOWED = 'WI', _('Widowed')
+        SEPERATED = 'SE', _('Seperated')
+
+    class AssociationChoices(models.TextChoices):
+        REGULAR = 'R', _('Regular')
+        VISITING = 'V', _('Visiting')
+        ADJUNCT = 'A', _('Adjunct')
+        CONTRACT = 'C', _('Contract')
+
+    faculty_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email_id = models.EmailField(max_length=100)
+    password = models.CharField(max_length=128)
+    department = models.CharField(max_length=100)
+    phone = models.BigIntegerField(null=True, blank=True)
+    date_of_joining = models.DateField(null=True, blank=True)
+    experience = models.IntegerField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=1, choices=GenderChoices.choices, null=True, blank=True)
+    marital_status = models.CharField(
+        max_length=50, choices=MartialStatusChoices.choices, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    teacher_picture = models.ImageField(null=True, blank=True)
+    designation = models.CharField(max_length=100, null=True, blank=True)
+
+    publications = models.ManyToManyField(Publications)
+    workshops = models.ManyToManyField(Workshops)
+    faculty_qualificatio
+
+    association_with_institution = models.CharField(
+        max_length=20, choices=AssociationChoices.choices, null=True, blank=True)
+    faculty_type = models.ManyToManyField(Type)
+    deleted = models.BooleanField(default=False, null=True, blank=True)
+    approved = models.BooleanField(default=False)
+    first_login = models.BooleanField(default=True)
+
+
+class LoginAuthKey(models.Model):
+    login_authkey_id = models.AutoField(primary_key=True)
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    authkey = models.CharField(max_length=128)
+    deleted = models.BooleanField(default=False)
+
+
 class Class(models.Model):
     class SemChoices(models.IntegerChoices):
         FIRST = 1, _('First')
@@ -127,7 +137,7 @@ class Class(models.Model):
     class_id = models.AutoField(primary_key=True)
     sem = models.IntegerField(choices=SemChoices.choices)
     sec = models.CharField(max_length=1)
-    graduation_year = models.PositiveSmallIntegerField(null = True)
+    graduation_year = models.PositiveSmallIntegerField(null=True)
     department = models.CharField(max_length=50)
     classteacher_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
