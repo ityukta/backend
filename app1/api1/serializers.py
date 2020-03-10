d52e47797cdf87c1d6883c78ab4845f295c0aaa0
kalpajpise <kalpaj12@gmail.com>
2020-03-10 20:44:03 +0530

    removed app1 django file

--- app1/api1/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import Faculty
-from rest_framework import serializers
-from .models import Faculty
-class InitialRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Faculty
-        fields = ['faculty_id', 'name', 'email_id', 'password', 'department', 'faculty_type']
-    
